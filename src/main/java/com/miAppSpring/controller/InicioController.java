8b28cc5817f2471a3a6ea736f2956a441b6b86d3
DanielSalazarW <danielh.sr20@gmail.com>
2021-07-12 22:14:36 -0500

    funcion de poder agreagr ususarios

--- src/main/java/com/miAppSpring/controller/InicioController.java
+++ src/main/java/com/miAppSpring/controller/InicioController.java
@@ -1,12 +1,14 @@
 package com.miAppSpring.controller;
 
 
+import com.miAppSpring.model.Usuario;
 import com.miAppSpring.servicios.UsuariosService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -17,19 +19,21 @@ public class InicioController {
 
     @GetMapping("/")
     public String inicio (Model model){
-
         var  usuarios = usuariosService.listaUsuarios();
-
-
-
-        // var usuarios = Arrays.asList(usuario,usuario2);
-
-
-
         var saludar = "listos para todo";
         log.info("se esta ejecutando inicio que retorna index");
         model.addAttribute("usuarios", usuarios);
         return "index";
+    }
+    @GetMapping("/agregar")
+    public String agregar (Usuario usuario){
+        return "modificar";
+    }
 
+    @PostMapping("/guardar")
+    public String guardar(Usuario usuario){
+
+        usuariosService.guardar(usuario);
+        return "redirect:/";
     }
 }
